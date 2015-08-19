706975b47918a151be3921eb3bfcd24ac14fc677
RossThomson <thomsonross@hotmail.com>
2015-08-19 16:41:28 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+		<meta http-equiv="refresh" content="60">
+		<link rel="stylesheet" href="styles/styles.css">
+		<title>Media Lynx</title>
+	</head>
+	<body>
+			
+			<header>
+			<h1 class="Title">
+				<a href="index.html">Media Lynx</a>
+			</h1>
+				<nav>
+					
+					<ul>
+						<li><a href="login.html">Login</a></li>
+					</ul>			
+				</nav>
+			</header>
+			<div>
+			<h2>Your personal media vault<h2>
+			<img class="main_pic" src="graphics/main.jpg" alt="Man wearing headphones"/>
+			</div>
+					<footer>
+					<nav class= "footer_nav">
+		
+						<a href="about.html">About</a>
+						<a href="help.html">Help</a>
+					
+					</nav>
+					</footer>
+	
+	</body>
+	</html>
