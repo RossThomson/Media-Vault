561a52d7d46b9e8f78a26e79166f7974fff9449e
jaeneungsim <simwithyou@gmail.com>
2015-08-25 00:19:41 +1000

    pages
    pages for about, help and sitemap

---
+++ help.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Help</title>
+<style type="text/css">
+html,body {
+	margin:0;
+	padding:0;
+}
+
+jae_body {
+	font-family: Arial, Helvetica, sans-serif;
+	text-align:center;
+	margin-top: 20px;
+}
+
+div#jae_content a, div#jae_extra a, div#jae_sidebar a {
+	color: #FFBA27;
+	font-weight: bold;
+	text-decoration: none;
+	background-color: #FAFAFA; 
+	border-bottom: 1px solid #F1F1F1; 
+	border-right: 1px solid #F1F1F1;
+	padding: 1px;
+}
+
+div#jae_content a:hover , div#jae_extra a:hover, div#jae_sidebar a:hover {
+	color: #FFBA27;
+	font-weight: bold;
+	text-decoration: none;
+	background-color: #FAFAFA; 
+	border-bottom: 1px solid #CCC; 
+	border-right: 1px solid #CCC;
+	padding: 1px;
+}
+
+div#jae_header {
+	padding-bottom: 5px;
+	border-bottom: 1px solid #EEE;
+}
+
+div#jae_header h1 {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 25px;
+	font-weight: normal;
+	color: #FFBA27;
+	letter-spacing: -1px;
+}
+
+div#jae_header h2 { /* The tagline thing */
+	margin: 0;
+	padding: 0;
+	font-size: 11px;
+	font-weight: normal;
+	color: #999;
+	padding-left: 20px;
+	margin-top: -15px;
+}
+
+#jae_navigation {
+font-family: "Lucida Grande", Tahoma, Arial, Helvetica, sans-serif;
+float: right;
+}
+
+#jae_navigation li {
+display: inline;
+padding-right: 20px;
+font-size: 11px;
+}
+
+#jae_navigation li a {
+font-size: 11px;
+color: #666;
+font-weight: normal;
+padding: 4px;
+text-decoration: none;
+}
+
+#jae_navigation li a:hover {
+text-decoration: underline;
+}
+
+div#jae_container {
+text-align:left
+}
+
+div#jae_content p, div#jae_extra p, div#jae_sidebar p {
+font-family: "Lucida Grande", Tahoma, Arial, Helvetica, sans-serif;
+font-size: 11px;
+color: #444;
+line-height:1.8em;
+}
+
+div#jae_content h1, div#jae_extra h1, div#jae_sidebar h1 {
+font-size: 14px;
+font-weight: normal;
+color: #FFBA27; 
+text-decoration: none; 
+padding: 3px 6px; 
+background-color: #F9F9F9; 
+border-bottom: 1px solid #F1F1F1; 
+border-right: 1px solid #F1F1F1;
+}
+
+div#jae_content h2 {
+font-family: "Lucida Grande", Tahoma, Arial, Helvetica, sans-serif;
+font-size: 11px;
+font-weight: bold;
+color: #444;
+margin: 0;
+padding: 0;
+}
+
+div#jae_footer p {
+margin:0;padding:5px 10px;
+}
+
+div#jae_container {
+width:700px;
+margin:0 auto;
+}
+
+div#jae_sidebar {
+float:right;
+width:330px;
+padding-bottom: 10px;
+}
+
+div#jae_extra {
+float:left;
+width:330px;
+}
+
+div#jae_footer {
+margin-top: 10px;
+background-color: #333;
+color: #FFF;
+font-size: 11px;
+font-family: Tahoma, Arial, Helvetica, sans-serif;
+clear:both;
+width:100%;
+padding: 5px;
+}
+
+div#jae_footer a, div#jae_footer a:hover {
+margin-top: 10px;
+background-color: #333;
+color: #FFF;
+font-size: 11px;
+font-family: Tahoma, Arial, Helvetica, sans-serif;
+clear:both;
+width:100%;
+color: #FFBA27;
+font-weight: bold;
+text-decoration: none;
+border-bottom: 1px solid #333; 
+border-right: 1px solid #333;
+}
+
+#jae_design-by {
+float: right;
+display:inline;
+}
+</style>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1"/>
+</head>
+<body>
+<div id="jae_container">
+	<div id="jae_navigation">
+		<ul>
+		<br><br><br>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="about.php">About Us</a></li>
+		<li><a href="sitemap.php">Sitemap</a></li>
+		</ul>
+	</div>
+		<A href="index.php">
+			<IMG src="./images/logo.jpg" width = "20%" height="20%"><br>
+		</A>
+				
+				<div id="jae_content">
+					<h1>FAQ</h1>
+					<h2>1. How can I upload media files?</h2>
+					<p>   Users can upload media files through.............................................</p>
+					<h2>2. I want to change my password.</h2>
+					<p>   Users can change passwords through.............................................</p>
+					<h2>3. How can I withdraw?</h2>
+					<p>   Users can withdraw through.............................................</p>
+					<h1>Resolved issues</h1>
+					<h2>1. Why can't I connect to Media Lynx?</h2>
+					<p>You may be experiencing an Internet connectivity problem that...........</p>
+					<h2>2. Media Lynx says my account is already in use when I try to watch.</h2>
+					<p>There are two common causes for this:..........</p>
+				</div>
+				
+<div id="jae_sidebar">
+	<h1>Contact us</h1>
+	<p>E-mail: xxxx@gmail.com</p>
+	<p>Phone: 180 xxx xxx </p>
+</div>
+<div id="jae_extra">
+	<h1>Extra Stuff </h1>
+	<p> If you have experienced .................................................................................... </p>
+</div>
+<div id="jae_footer">
+
+	<span id="jae_design-by">Design by Media lynx</span> 
+	  Copyright &copy; Media Lynx 2015.
+</div>
+</div>
+</body>
+</html>
+
+<!-- Design by Smallpark.org. Released as open source. -->
