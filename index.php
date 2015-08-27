f9eaa492e68d5076d82965d3defc7fb42f6161d4
Ross <thomson360@gmail.com>
2015-08-27 17:34:01 +1000

    Front end website files

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+		<meta http-equiv="refresh" content="60">
+		<link rel="stylesheet" href="styles/styles.css">
+		<title>Media Lynx</title>
+	</head>
+	<body class = background_picture>
+			
+			<header>
+			<h1 class="Title">
+			<a href="index.php"><img src="graphics/logo.jpg"></a>
+			
+			</h1>
+				<nav>
+					
+					<ul>
+						<li><a href="login.php">Login</a></li>
+					</ul>			
+				</nav>
+			</header>
+			<div>
+			<h2>Your personal media vault<h2>
+			<!--<img class="main_pic" src="graphics/main.jpg" alt="Man wearing headphones"/>-->
+			</div>
+					<footer>
+					<nav class= "footer_nav">
+		
+						<a href="about.php">About</a>
+						<a href="help.php">Help</a>
+					
+					</nav>
+					</footer>
+	
+	</body>
+	</html>
