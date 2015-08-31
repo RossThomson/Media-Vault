5e71a9fae673f2e6f3e6efb2ded91e9dc6b9c7d0
jaeneungsim <simwithyou@gmail.com>
2015-09-01 02:29:13 +1000

    main medai page & changed  css
    changed

---
+++ media.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+    <head>
+        
+        <title>Media</title>
+        
+        <meta name="Author" content="Mike Ruckman" />
+        <meta name="About" content="A free design for OSWD.org" />
+        <meta name="Copy-Up" content="Mike Ruckman" />
+        <meta name="Designed By" content="Theruckman.com" />
+        
+        <meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
+     	<link rel="stylesheet" href="styles/styles.css">
+        
+    </head>
+
+    <body>
+	<div class="wrapper">
+	<header>
+		<nav>
+			<a href="index.php"><img src="graphics/logo.jpg"></a>
+			<ul>
+				<li><a href="login.php">Login</a></li>
+				<li><a href="about.php">About</a></li>
+				<li><a href="help.php">Help</a></li>
+			</ul>			
+		</nav>
+	</header>
+
+    <div id="media_main_wrapper">
+    <div id="media_main_body">
+    <div id="media_main_gMenu">
+       <ul>
+       <li><a href="#">Movie</a></li>
+       <li><a href="#">Photo</a></li>
+       <li><a href="#">Document</a></li>
+       <li><a href="#">Music</a></li><br><br><br>
+       <li><a href="#">Upload</a></li> 
+       </ul>
+    </div>
+                
+    <div id="media_main_media">
+       <h2>Click the menu on the left</h2>
+       <p>Click the menu on the left to see, watch and modify your media files </p>
+     
+       <h2>Click the upload button</h2>
+       <p>You can upload your media files into your personal storage conveniently</p> 
+                
+       <h2>Automatic classification</h2>
+       <p>Uploaded media files are classified automatically depending on the file type</p>      
+                       
+    </div>
+       <footer class="footer_relative">
+	   <span id="jae_design-by">Design by Media lynx</span> 
+		Copyright &copy; Media Lynx 2015.
+		</footer>
+    </body>
+</html>
