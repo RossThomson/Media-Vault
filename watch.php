a49e77f399528ec76ab545138f9d29436021721d
jaeneungsim <simwithyou@gmail.com>
2015-09-04 13:22:14 +1000

    change
    change

--- watch.php
+++ watch.php
@@ -1,18 +1,32 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['email'])){
+header("location: Login.php");
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
 <meta charset="utf-8">
 <title>Watching</title>
 </head>
 
 <body>
-<div id="aboutus_content">	
+<div class="wrapper">
+	<header>
+		<?php include 'header.php'; ?>
+	</header>
+	<div id="aboutus_content">	
 	<br><br>
 	<a href="media_movie.php">movie</a> &nbsp <a href="media_document.php">document</a> &nbsp <a href="media_photo.php">photo</a> &nbsp <a href="media_music.php">music</a>
+	</div>
+	<video align="center" src="epl.mp4" width="500" height="350" controls>
+	</video>
+	<br><br>
 </div>
-<video align="center" src="epl.mp4" width="500" height="350" controls>
-</video>
-<br><br>
 <footer class="footer_relative">
 	<span id="jae_design-by">Design by Media lynx</span> 
 		Copyright &copy; Media Lynx 2015.
