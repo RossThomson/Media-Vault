6beb57eb3db4128278c2a970ad864eb2e27f9d14
ChristophBrinkmann <christoph.brinkmann@connect.qut.edu.au>
2015-09-04 16:32:49 +1000

    bug fixing test

--- password_recovery.php
+++ password_recovery.php
@@ -1,16 +1,16 @@
-/*<?php
-session_start();
-
-if(!isset($_SESSION['email'])){
-header("location: Login.php");
-}
-?>*/
+<?php//
+//session_start();
+//
+//if(!isset($_SESSION['email'])){
+//header("location: Login.php");
+//}
+//?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
     <head>
         
-        <title>Media</title>
+        <title>Password recovery</title>
         
         <meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
      	<link rel="stylesheet" href="styles/styles.css">
