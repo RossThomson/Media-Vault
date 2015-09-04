357c9aac19041bb2a0003ec98aa28033666de91f
jaeneungsim <simwithyou@gmail.com>
2015-09-04 13:44:23 +1000

    changed
    changed

--- watch.php
+++ watch.php
@@ -10,16 +10,17 @@ header("location: Login.php");
 <html>
 <head>
 <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
-<meta charset="utf-8">
+<!--<meta charset="utf-8">-->
 <title>Watching</title>
 <link rel="stylesheet" href="styles/styles.css">
 </head>
-
 <body>
+
 <div class="wrapper">
 	<header>
 		<?php include 'header.php'; ?>
 	</header>
+	
 	<div id="aboutus_content">	
 	<br><br>
 	<a href="media_movie.php">movie</a> &nbsp <a href="media_document.php">document</a> &nbsp <a href="media_photo.php">photo</a> &nbsp <a href="media_music.php">music</a>
