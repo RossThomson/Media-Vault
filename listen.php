459e2b990d5c3da23dcd06be2419a18a828c31bc
jaeneungsim <simwithyou@gmail.com>
2015-09-04 14:00:37 +1000

    changed
    changed

--- listen.php
+++ listen.php
@@ -1,18 +1,33 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['email'])){
+header("location: Login.php");
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
 <title>Listening</title>
+
+<link rel="stylesheet" href="styles/styles.css">
 </head>
 
 <body>
-<div id="aboutus_content">	
+<div class="wrapper">
+	<header>
+		<?php include 'header.php'; ?>
+	</header>
+	
+	<div id="aboutus_content">	
 	<br><br>
 	<a href="media_movie.php">movie</a> &nbsp <a href="media_document.php">document</a> &nbsp <a href="media_photo.php">photo</a> &nbsp <a href="media_music.php">music</a>
-</div>
-<audio src="test.mp3" controls>
-</audio>
-<br><br>
+	</div>
+	<audio src="test.mp3" controls>
+	</audio>
+	<br><br>
 <footer class="footer_relative">
 	<span id="jae_design-by">Design by Media lynx</span> 
 		Copyright &copy; Media Lynx 2015.
