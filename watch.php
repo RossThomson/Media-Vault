dadaf7f6e15622ea39ffc1c00a967a4903eb7140
jaeneungsim <simwithyou@gmail.com>
2015-09-04 13:49:49 +1000

    changed
    changed

--- watch.php
+++ watch.php
@@ -10,11 +10,12 @@ header("location: Login.php");
 <html>
 <head>
 <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="styles/styles.css">
 <!--<meta charset="utf-8">-->
 <title>Watching</title>
-<link rel="stylesheet" href="styles/styles.css">
 </head>
 <body>
+<div class="top_bar">
 
 <div class="wrapper">
 	<header>
