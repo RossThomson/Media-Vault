2edcdde2a88c7568d1c1197f10b09fbf2b8a0c68
Ross <thomson360@gmail.com>
2015-09-04 12:12:34 +1000

    testing session

---
+++ header.php
@@ -0,0 +1,32 @@
+	
+<?php	
+
+	if(isset($_SESSION['email'])) 
+	
+	{
+		$login = "Logout";
+	
+	}
+	
+	else 
+	{
+		$login = "Login";
+		
+	}
+
+
+	
+	?>
+	
+	<nav>
+			<a href="index.php"><img src="graphics/logo.jpg"></a>
+			<ul>
+				<li><a href="<?php echo $login?>.php"><?php echo $login;?></a></li>
+				<li><a href="about.php">About</a></li>
+				<li><a href="help.php">Help</a></li>
+			</ul>			
+	</nav>
+
+
+
+
