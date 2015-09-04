b5a45f5d92548b1efcd6f3347573cbc1ab7063ea
ChristophBrinkmann <christoph.brinkmann@connect.qut.edu.au>
2015-09-04 13:37:21 +1000

    little work done with Delete. Nothing much to note.

--- Delete.php
+++ Delete.php
@@ -1,14 +1,15 @@
 <?php
 	//Assuming we already have a connection opened (under conn) and we also have the user 
 	//logged into their account and looking at their file.
+	
+	
 	$userTable = "table";
 	$filename = "FileName";
 	
 	$sql = "DELETE FROM " $userTable " WHERE File = "$filename;
 	if($conn -> query($sql) === TRUE) {
-		//javascript or html code to say that it has been successfully deleted.
+		echo "Item has been deleted successfully";
 	} else {
-		//javascript/html to say it wasn't
+		echo "Item was not deleted successfully";
 	}
-	$conn -> close();
 ?>
