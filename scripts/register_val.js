c95adf442b24a922d85c03c619424e3a905109b3
Ross <thomson360@gmail.com>
2015-09-08 10:45:42 +1000

    Testing register validation

--- scripts/register_val.js
+++ scripts/register_val.js
@@ -38,7 +38,15 @@ function checkEmail() {
     var email = document.getElementById("email");
     var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 	var message = document.getElementById("emailmessage");
+	var goodColor = "#66cc66";
+    var badColor = "#ff6666";
     if (!filter.test(email.value)) {
-     message.innerHTML = "Invalid email address"
+     message.innerHTML = "Invalid email address";
+	  message.style.color = badColor;
+ }
+ 
+ else {
+	  message.innerHTML = "Valid email address";
+	  message.style.color = goodColor;
  }
 }
