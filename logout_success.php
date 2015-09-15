1ee53d647b6501b92f3420fcf8c3fdda90323682
Ross <thomson360@gmail.com>
2015-09-15 10:26:07 +1000

    Created Page for confirm user has logged out

---
+++ logout_success.php
@@ -0,0 +1,31 @@
+
+
+
+
+<!DOCTYPE html>
+
+<html id="Homepage_pic">
+    <head>
+        <meta charset="utf-8">
+		<meta http-equiv="refresh" content="60">
+		<link rel="stylesheet" href="styles/styles.css">
+		<title>Media Lynx</title>
+	</head>
+	<body>
+	<div class="wrapper">
+			
+			<header>
+				<?php include 'header.php'; ?>
+			</header>
+
+			<div id = "register_success">
+			<h2> Logged out, See you next time!</h2>
+			</div>
+					<footer class="footer_absolute">
+					<span id="jae_design-by">Design by Media lynx</span> 
+						Copyright &copy; Media Lynx 2015.
+					</footer>
+		<?php  header('Refresh: 6; URL= index.php'); ?>	
+	</div>
+	</body>
+	</html>
