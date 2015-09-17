a4e08eddc2187a92ca884354b8d2f000dea8956f
ChristophBrinkmann <christoph.brinkmann@connect.qut.edu.au>
2015-09-17 20:18:09 +1000

    Ok, here are the delete changes...

--- Delete.php
+++ Delete.php
@@ -33,21 +33,37 @@ header("location: Login.php");
 	</tr>
 	
 	<?php
-		$allFiles = array();
-	
-		foreach ((new DirectoryIterator(dirname(__FILE__))) as $file) {
-			$allFiles[] = $file->getFilename();
+		$dbname = "Media_Lynx";
+		$dbserver = "localhost";
+		$dbuser = "root";
+		$dbpass = "root";
+		$Email = $_SESSION['email'];
+		
+		$conn = new mysqli($dbserver,$dbuser,$dbpass,$dbname);
+		if(!$conn->connect_error) {
+			die("Connection failed".$conn->connect_error);
 		}
-
-		foreach($allFiles as $value) {
-			count = 0;
-			echo '<tr>';
-			echo '<td><input type = "checkbox" name = "$count">'.$value.'</td>';
-			echo '</tr>';
-			count = count + 1;
+			
+		$sql = "SELECT USERID FROM User WHERE email = '$Email'";
+		$user = $conn->query($sql);
+		
+		$newSql = "SELECT * FROM CONTENT WHERE USERID = '$user'";
+		$result = $conn->query($newSql);
+		
+		if($result->num_rows > 0) {
+			
+			while($row = $result->fetch_assoc()) {
+				$id = $row["CONTENTID"];
+				echo "<tr>";
+				echo '	<td><input type = "checkbox" name = "$id">'.$row["CONTENTTITLE"].'</td>';
+				echo '	<td>'.$row["SYNOPSIS"].'</td>';
+				echo '</tr>';
+			} 
 		}
+		
+		$conn->close();
 	?>
-	
+	</table>
 	
 	
 	
@@ -55,6 +71,5 @@ header("location: Login.php");
 		<span id="jae_design-by">Design by Media lynx</span> 
 		Copyright &copy; Media Lynx 2015.
 	</footer>
-	</div>
 	</body>
 </html>
