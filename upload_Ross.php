6fb590b380fd747cee5ed7d2514bdb8b439b10dc
Ross <thomson360@gmail.com>
2015-09-18 14:56:11 +1000

    Testing upload

--- upload_Ross.php
+++ upload_Ross.php
@@ -38,7 +38,7 @@ if(isset($_POST["submit"])) {
 					
 					
 					 
-					$sql = "INSERT INTO CONTENT(USERID, CONTENTTITLE, CONTENTTYPE, SIZE, SYNOPSIS, PICTURES) VALUES ('$userid','$filename','$filetype','$filesize','$synopsis', " . $db->quote($imageData) . ")");
+					$sql = "INSERT INTO CONTENT(USERID, CONTENTTITLE, CONTENTTYPE, SIZE, SYNOPSIS, PICTURES) VALUES ('$userid','$filename','$filetype','$filesize','$synopsis')";
 			
 						
 					$pdo->exec($sql);
