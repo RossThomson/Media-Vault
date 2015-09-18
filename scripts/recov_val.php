9e7e52224fed43741551faa19aaef00670f346c4
ChristophBrinkmann <christoph.brinkmann@connect.qut.edu.au>
2015-09-19 09:40:42 +1000

    testy things

--- scripts/recov_val.php
+++ scripts/recov_val.php
@@ -4,15 +4,16 @@ if(isset($_SESSION['email'])){
 header("location: media.php");
 }
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
     <head>
         
-        <title>Password recovery</title>
+        <title>Password Reset</title>
         
         <meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
-     	<link rel="stylesheet" href="styles/styles.css">
-		<script type="text/javascript" src="scripts/login_val.js"></script>
+     	<link rel="stylesheet" href="../styles/styles.css">
+		<script type="text/javascript" src="login_val.js"></script>
         
     </head>
 
