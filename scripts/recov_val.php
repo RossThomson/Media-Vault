0120fd020a56364ff91b1e038bf580ec8a067e98
ChristophBrinkmann <christoph.brinkmann@connect.qut.edu.au>
2015-09-18 16:33:27 +1000

    let us see if this works

--- scripts/recov_val.php
+++ scripts/recov_val.php
@@ -22,7 +22,7 @@
 			echo'<p> this explains everything</p>';
 		}else{
 		//$result2 = $result->fetch_array();
-		echo '<p>Ok, this is the result: '.$result['SECRETQUESTION'].'...stuff</p>';
+		echo '<p>Ok, this is the result: '.$result.'...stuff</p>';
 		echo '<p>Also, the email is: '.$email.'</p>';
 		echo '<p>AND sql thingy too: '.$sql.'</p>';
 		$test = $result->fetch();
