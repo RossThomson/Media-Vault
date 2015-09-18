95d486e4a65fb03f2830bd748caa66bd6ed60867
ChristophBrinkmann <christoph.brinkmann@connect.qut.edu.au>
2015-09-18 13:18:53 +1000

    testing testing testing

--- scripts/recov_val.php
+++ scripts/recov_val.php
@@ -1,31 +1,4 @@
 <?php
-if(isset($_SESSION['email'])){
-header("location: media.php");
-}
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
-    <head>
-        
-        <title>Password recovery</title>
-        
-        <meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
-     	<link rel="stylesheet" href="styles/styles.css">
-		<script type="text/javascript" src="scripts/login_val.js"></script>
-        
-    </head>
-
-    <body>
-	<div class="top_bar">
-	
-	
-	<div class="wrapper">
-	<header>
-		<?php include 'header.php'; ?>
-	</header>
-
-<?php
 	$dbname = "Media_Lynx";
 	$dbserver = "54.79.17.142";
 	$dbuser = "root";
