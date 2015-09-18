8d25ec3571a92bed6c6dbfd6d6f2fec861c938cc
ChristophBrinkmann <christoph.brinkmann@connect.qut.edu.au>
2015-09-19 07:31:06 +1000

    testing

--- scripts/recov_val.php
+++ scripts/recov_val.php
@@ -25,48 +25,49 @@ header("location: media.php");
 		<?php include 'header.php'; ?>
 	</header>
 
-<?php
-	$dbname = "MEDIALYNX";
-	$dbhost = "localhost";
-	$dbuser = "root";
-	$dbpass = "root";
+	<?php
+		$dbname = "MEDIALYNX";
+		$dbhost = "localhost";
+		$dbuser = "root";
+		$dbpass = "root";
 	
-	$email = "";
+		$email = "";
+			
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	
-		$email = $_POST['email'];
+			$email = $_POST['email'];
+			
+			$conn = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
 		
-		$conn = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
-	
-		$q1 = $conn->query("SELECT * FROM USERS WHERE EMAIL = '$email'");
-		$q2 = $q1->fetch(); 
-		
-		echo "<p> working here </p>";
-		echo "<p>".$q2['SECRETQUESTION']."</p>";
-		echo "<p>".$q2['SECRETANSWERS']."</p>";
-		
-		if($q2['EMAIL'] === null) {
-			echo '<p>You are not a user,'.$email.'. Please <a href = "../register.php"> Register </a>to use our services.</p>';
-		} else {
-			echo '<p>It is working</p>';
-			echo '<form id = "RenewForm" name = "renew_form" method = "POST">';
-			echo '<label>Secret Question</label><br>';
-			echo '<label>'.$q2['SECRETQUESTION'].'</label><br>';
-			echo '<label>Secret Answer</label><br><input type = "password" id = "newpass" name = "answer"><br>';
-			echo '<input class="btn btn-alt" type = "submit" name = "submit" value = "Submit">';
-			$answer = $_POST["answer"];
-			if($answer !== $row['SECRETANSWERS']) {
-				echo "Not the correct answer";
+			$q1 = $conn->query("SELECT * FROM USERS WHERE EMAIL = '$email'");
+			$q2 = $q1->fetch(); 
+			
+			echo "<p> working here </p>";
+			echo "<p>".$q2['SECRETQUESTION']."</p>";
+			echo "<p>".$q2['SECRETANSWERS']."</p>";
+			
+			if($q2['EMAIL'] === null) {
+				echo '<p>You are not a user,'.$email.'. Please <a href = "../register.php"> Register </a>to use our services.</p>';
 			} else {
-				echo 'Time to reset your password, but I am figuring that one out';
+				echo '<p>It is working</p>';
+				echo '<form id = "RenewForm" name = "renew_form" method = "POST">';
+				echo '<label>Secret Question</label><br>';
+				echo '<label>'.$q2['SECRETQUESTION'].'</label><br>';
+				echo '<label>Secret Answer</label><br><input type = "password" id = "newpass" name = "answer"><br>';
+				echo '<input class="btn btn-alt" type = "submit" name = "submit" value = "Submit">';
+				echo '</form>'
+				$answer = $_POST["answer"];
+				if($answer !== $row['SECRETANSWERS']) {
+					echo "Not the correct answer";
+				} else {
+					echo 'Time to reset your password, but I am figuring that one out';
+				}
 			}
+			
+			$conn->close();
 		}
-		
-		$conn->close();
-	}
-?>
-</body>
+	?>
+	</body>
 <footer class="footer_absolute">
 		<span id="jae_design-by">Design by Media lynx</span> 
 		Copyright &copy; Media Lynx 2015.
