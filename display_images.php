4ac74a7203e156a2d4c33f2bca45c2a27eefa0ea
Ross <thomson360@gmail.com>
2015-09-19 16:56:42 +1000

    Testing Image display

--- display_images.php
+++ display_images.php
@@ -9,19 +9,20 @@ mysql_connect($dbhost, $dbuser, $dbpass) or die("Can not connect to database: ".
 
 mysql_select_db($dbname) or die("Can not select the database: ".mysql_error());
 
-//Session_start();
-//$id = $_$_SESSION['userid'];
-$id = '6';
+Session_start();
+$id = $_$_SESSION['userid'];
 
 
 
-$query = mysql_query("SELECT * FROM CONTENT WHERE CONTENTID='".$id."'");
-$row = mysql_fetch_array($query);
+
+$query = mysql_query("SELECT * FROM CONTENT WHERE USERID='".$id."'");
+ while $row = mysql_fetch_array($query) {
 $content = $row['PICTURES'];
 $imagetype = $row['CONTENTTYPE'];
 
 header('Content-type: "$imagetype"');
      echo $content;
+ }
 
 
 ?>
