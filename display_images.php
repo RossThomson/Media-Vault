b0d7d496e5e0796fad6d25302ac39cdb582e05c2
Ross <thomson360@gmail.com>
2015-09-19 17:41:24 +1000

    Testing Image display

--- display_images.php
+++ display_images.php
@@ -18,9 +18,9 @@ $id = $_SESSION['userid'];
 $query = mysql_query("SELECT * FROM CONTENT WHERE USERID='".$id."'");
  while($row = mysql_fetch_array($query)) {
 
-//$imagetype = $row['CONTENTTYPE'];
+$imagetype = $row['CONTENTTYPE'];
 
-header('Content-type: ''$row["CONTENTTYPE"]'');
+header('Content-type: "$imagetype"');
      //echo $row['PICTURES'];
 	  echo "<img src='php/imgView.php?imgId=".$row['PICTURES']."' />";
  }
