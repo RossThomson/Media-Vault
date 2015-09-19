22418eedc047ee2fef8a316ff5034268f4a8bbb4
Ross <thomson360@gmail.com>
2015-09-19 17:11:01 +1000

    Testing upload of images

--- display_images.php
+++ display_images.php
@@ -9,13 +9,13 @@ mysql_connect($dbhost, $dbuser, $dbpass) or die("Can not connect to database: ".
 
 mysql_select_db($dbname) or die("Can not select the database: ".mysql_error());
 
-//Session_start();
-//$id = $_$_SESSION['userid'];
-$id = '6';
+Session_start();
+$id = $_$_SESSION['userid'];
+//$id = '6';
 
 
 
-$query = mysql_query("SELECT * FROM CONTENT WHERE CONTENTID='".$id."'");
+$query = mysql_query("SELECT * FROM CONTENT WHERE USERID='".$id."'");
  while($row = mysql_fetch_array($query)) {
 $content = $row['PICTURES'];
 $imagetype = $row['CONTENTTYPE'];
