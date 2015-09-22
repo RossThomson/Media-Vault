b625ffc63c724ceca3765873dc8eb35a351c156e
Ross <thomson360@gmail.com>
2015-09-22 17:18:56 +1000

    Testing upload images

--- upload_Ross_two.php
+++ upload_Ross_two.php
@@ -8,9 +8,11 @@
 
 
 $currentdir = getcwd();
-echo '$currentdir';
+echo "$currentdir";
 $target = $currentdir .'/uploads/' . basename($_FILES['photo']['name']);
 move_uploaded_file($_FILES['photo']['tmp_name'], $target);
+$Check = $_FILES[“myFile”][“error”];
+echo "$Check";
 
 
 
