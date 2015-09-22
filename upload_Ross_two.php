1ca2b541871026fc275fcbf07b3388c44f783edd
Ross <thomson360@gmail.com>
2015-09-22 16:43:13 +1000

    Testing upload images

--- upload_Ross_two.php
+++ upload_Ross_two.php
@@ -6,7 +6,7 @@ if($_FILES['photo']['name'])
 	if(!$_FILES['photo']['error'])
 	{
 		//now is the time to modify the future file name and validate the file
-		$new_file_name = strtolower($_FILES['photo']['tmp_name']); //rename file
+		$new_file_name = ($_FILES['photo']['tmp_name']); //rename file
 		if($_FILES['photo']['size'] > (1024000)) //can't be larger than 1 MB
 		{
 			$valid_file = false;
@@ -16,8 +16,9 @@ if($_FILES['photo']['name'])
 		//if the file has passed the test
 		if($valid_file)
 		{
-			//move it to where we want it to be
-			move_uploaded_file($_FILES['photo']['tmp_name'], 'uploads/'.$new_file_name);
+			$currentdir = getcwd();
+			$target = $currentdir .'/uploads/' . basename($_FILES['photo']['name']);
+			move_uploaded_file($_FILES['photo']['tmp_name'], $target);
 			$message = 'Congratulations!  Your file was accepted.';
 		}
 	}
