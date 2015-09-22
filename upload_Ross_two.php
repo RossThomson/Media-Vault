b7e34ff5d94a97c22371ef5132319c805fe192ff
Ross <thomson360@gmail.com>
2015-09-22 16:06:09 +1000

    Testing upload images

--- upload_Ross_two.php
+++ upload_Ross_two.php
@@ -5,7 +5,7 @@
 	{
 		//now is the time to modify the future file name and validate the file
 		$new_file_name = strtolower($_FILES["fileToUpload"]["name"]); //rename file
-		if($_FILES['fileToUpload']['size'] > (1024000)) //can't be larger than 1 MB
+		if($_FILES["fileToUpload"]["size"] > (1024000)) //can't be larger than 1 MB
 		{
 			$valid_file = false;
 			$message = 'Oops!  Your file\'s size is to large.';
@@ -15,16 +15,19 @@
 		if($valid_file)
 		{
 			//move it to where we want it to be
-			move_uploaded_file($_FILES['fileToUpload']['tmp_name'], 'uploads/'.$new_file_name);
+			move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], 'uploads/'.$new_file_name);
 			$message = 'Congratulations!  Your file was accepted.';
 		}
-	}
-	//if there is an error...
-	else
+		
+		else
 	{
 		//set that to be the returned message
-		$message = 'Ooops!  Your upload triggered the following error:  '.$_FILES['fileToUpload']['error'];
+		$message = 'Ooops!  Your upload triggered the following error:  '.$_FILES["fileToUpload"]['error'];
 	}
+	
+	}
+	//if there is an error...
+	
 
 
 ?>
