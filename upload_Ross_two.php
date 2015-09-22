da400d3dc3ab1624dd1eb9d324b1b5724ee3bdd8
Ross <thomson360@gmail.com>
2015-09-22 16:18:50 +1000

    Testing upload images

--- upload_Ross_two.php
+++ upload_Ross_two.php
@@ -2,8 +2,9 @@
 
 	
 			//move it to where we want it to be
+			
 			move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], "uploads/".$new_file_name);
 			echo "Congratulations!  Your file was accepted.";
-]
+
 
 ?>
