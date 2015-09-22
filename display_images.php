46d0807caf6dd975764319ae679e6f55d0e405b3
Ross <thomson360@gmail.com>
2015-09-22 10:34:05 +1000

    Testing display images

--- display_images.php
+++ display_images.php
@@ -21,32 +21,6 @@ header('Content-type: "$imagetype"');
 
 	 
 	  echo $row['PICTURES'];
- }<?php
-
-	$dbhost = "localhost";
-	$dbname	= "MEDIALYNX";
-	$dbuser	= "root";
-	$dbpass	= "root";
-
-mysql_connect($dbhost, $dbuser, $dbpass) or die("Can not connect to database: ".mysql_error());
-
-mysql_select_db($dbname) or die("Can not select the database: ".mysql_error());
-
-Session_start();
-$id = $_SESSION['userid'];
-
-$query = mysql_query("SELECT * FROM CONTENT WHERE USERID='".$id."'");
- while($row = mysql_fetch_array($query)) {
-
-$imagetype = $row['CONTENTTYPE'];
-
-header('Content-type: "$imagetype"');
-
-	
-	  echo $row['PICTURES'];
  }
+ ?>
 
-?>
-
-
-?>
