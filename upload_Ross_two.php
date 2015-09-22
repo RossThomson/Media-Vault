55129ed6dee1bf89070f2c6a9e8a925b88f6f760
Ross <thomson360@gmail.com>
2015-09-22 17:21:56 +1000

    Testing upload images

--- upload_Ross_two.php
+++ upload_Ross_two.php
@@ -9,7 +9,7 @@
 
 $currentdir = getcwd();
 echo "$currentdir";
-$target = $currentdir .'/uploads/' . basename($_FILES['photo']['name']);
+$target = "http://54.79.17.142/Media-Vault/uploads/" . basename($_FILES['photo']['name']);
 move_uploaded_file($_FILES['photo']['tmp_name'], $target);
 $Check = $_FILES[“myFile”][“error”];
 echo "$Check";
