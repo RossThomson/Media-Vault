acc416b944e8b0c6d757324721d0c01fc7533ebd
Ross <thomson360@gmail.com>
2015-09-22 16:29:15 +1000

    Testing upload images

--- upload_Ross_two.php
+++ upload_Ross_two.php
@@ -3,26 +3,9 @@
 	
 
 		
-		$new_file_name = strtolower($_FILES["fileToUpload"]["name"]); 
-		if($_FILES["fileToUpload"]["size"] > (1024000)) //can't be larger than 1 MB
-		{
-			$valid_file = false;
-			echo "Oops!  Your file\'s size is to large.";
-		}
-		
-		
-		if($valid_file)
-		{
+		$new_file_name = strtolower($_FILES["fileToUpload"]["name"]); 		
+
 			//move it to where we want it to be
 			move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], "uploads/".$new_file_name);
 			echo "Congratulations!  Your file was accepted.";
-		}
-		
-
-	
-	
-
-	
-
-
 ?>
