527a882171dd7f4cf01df032537d2a62b041e281
Ross <thomson360@gmail.com>
2015-09-22 16:31:24 +1000

    Testing upload images

--- upload_Ross_two.php
+++ upload_Ross_two.php
@@ -6,6 +6,6 @@
 		$new_file_name = strtolower($_FILES["fileToUpload"]["name"]); 		
 
 			//move it to where we want it to be
-			move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], "uploads/".$new_file_name);
+			move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], "uploads/test");
 			echo "Congratulations!  Your file was accepted.";
 ?>
