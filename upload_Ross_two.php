da46d77ffb09a55c4729eb839a98eec5ad6bfb8d
Ross <thomson360@gmail.com>
2015-09-22 17:49:19 +1000

    Testing upload images

--- upload_Ross_two.php
+++ upload_Ross_two.php
@@ -2,17 +2,15 @@
 <?php
 
 	
-	
+	$target_path = "uploads/";
 
+$target_path = $target_path . basename( $_FILES['photo']['name']); 
 
-
-
-$target = '/uploads/' . basename($_FILES['photo']['name']);
-move_uploaded_file($_FILES['photo']['tmp_name'], $target);
-
-
-
-
-
+if(move_uploaded_file($_FILES['photo']['tmp_name'], $target_path)) {
+    echo "The file ".  basename( $_FILES['photo']['name']). 
+    " has been uploaded";
+} else{
+    echo "There was an error uploading the file, please try again!";
+}
 
 ?>
