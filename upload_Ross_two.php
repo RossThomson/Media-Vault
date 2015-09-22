3f62d833cb18492817a941e35402896ce63c4e46
Ross <thomson360@gmail.com>
2015-09-22 16:17:03 +1000

    Testing upload images

--- upload_Ross_two.php
+++ upload_Ross_two.php
@@ -1,29 +1,9 @@
 <?php
 
 	
-	if(isset($_POST["submit"]))
-	{
-		//now is the time to modify the future file name and validate the file
-		$new_file_name = strtolower($_FILES["fileToUpload"]["name"]); //rename file
-		if($_FILES["fileToUpload"]["size"] > (1024000)) //can't be larger than 1 MB
-		{
-			$valid_file = false;
-			echo "Oops!  Your file\'s size is to large.";
-		}
-		
-		//if the file has passed the test
-		if($valid_file)
-		{
 			//move it to where we want it to be
 			move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], "uploads/".$new_file_name);
 			echo "Congratulations!  Your file was accepted.";
-		}
-		
-
-	
-	}
-	//if there is an error...
-	
-
+]
 
 ?>
