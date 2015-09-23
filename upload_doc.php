0ccc666cf6d1d5dd2fb46cee2f5046b0f8342a62
jaeneungsim <simwithyou@gmail.com>
2015-09-23 23:58:54 +1000

    ref
    ref

--- upload_doc.php
+++ upload_doc.php
@@ -43,12 +43,12 @@ if ($uploadOk == 0) {
     }
 }
 
-/* if(isset($_POST["submit"])) {
-    //$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-	//$imageData = file_get_contents($_FILES["fileToUpload"]["tmp_name"]);
-    //if($check !== false) {
-        //echo "File is an image - " . $check["mime"] . ".";
-        //$uploadOk = 1; */
+if(isset($_POST["submit"])) {
+    /* $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+	$imageData = file_get_contents($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1; */
 		
 	$dbhost = "localhost";
 	$dbname	= "MEDIALYNX";
@@ -61,38 +61,27 @@ if ($uploadOk == 0) {
 	$synopsis = "description";
 
 		try {
-			
-			
-			
-			
-					$pdo = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
+			$pdo = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
 		
+			$sql = "INSERT INTO CONTENT(USERID, CONTENTTITLE, CONTENTTYPE, SIZE, SYNOPSIS) VALUES ('$userid','$filename','$FileType','$filesize','$synopsis')";
 			
-					
-					
-					 
-					$sql = "INSERT INTO CONTENT(USERID, CONTENTTITLE, CONTENTTYPE, SIZE, SYNOPSIS) VALUES ('$userid','$filename','$FileType','$filesize','$synopsis')";
-			
-					include 'library/closedb.php';
-					$pdo->exec($sql);
-					header("location: test2.php");
-						
-					 
-				}
+			include 'library/closedb.php';
+			$pdo->exec($sql);
+			header("location: test2.php");
+		}
 				
-			catch(PDOException $e)
-				{
-					echo $e->getMessage();
-				}
+		catch(PDOException $e){
+			echo $e->getMessage();
+		}
 
-				$pdo = null;
+		$pdo = null;
 	
-    //} 
+} 
 
 
  /* else {
         echo "File is not an image.";
         $uploadOk = 0;
     }	 */	
-}
+
 ?>
