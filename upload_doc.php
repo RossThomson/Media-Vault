8e0f7a841f04ed4a241462ffa6f3149afbbe6453
jaeneungsim <simwithyou@gmail.com>
2015-09-23 18:56:14 +1000

    test
    test

--- upload_doc.php
+++ upload_doc.php
@@ -2,7 +2,7 @@
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["doc"]["name"]);
 $uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$FileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
 /* if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["doc"]["tmp_name"]);
@@ -25,7 +25,7 @@ if ($_FILES["doc"]["size"] > 500000) {
     $uploadOk = 0;
 }
 // Allow certain file formats
-if($imageFileType != "doc" && $imageFileType != "txt" && $imageFileType != "pdf") {
+if($FileType != "doc" && $FileType != "txt" && $FileType != "pdf") {
     echo "Sorry, only DOC, TXT & PDF files are allowed.";
     $uploadOk = 0;
 }
