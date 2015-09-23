258608b289408f63dc9445758d15ffd5c01ac856
jaeneungsim <simwithyou@gmail.com>
2015-09-24 00:38:16 +1000

    ref
    ref

--- upload_doc.php
+++ upload_doc.php
@@ -1,13 +1,20 @@
 <?php
 
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileName"]["name"]);
+$uploadOk = 1;
+$FileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$filename = basename($_FILES["fileName"]["name"]);
+$filesize = $_FILES["fileName"]["size"];
+
 if(isset($_POST["submit"])) {
 	
-	$target_dir = "uploads/";
+	/* $target_dir = "uploads/";
 	$target_file = $target_dir . basename($_FILES["fileName"]["name"]);
 	$uploadOk = 1;
 	$FileType = pathinfo($target_file,PATHINFO_EXTENSION);
 	$filename = basename($_FILES["fileName"]["name"]);
-	$filesize = $_FILES["fileName"]["size"];
+	$filesize = $_FILES["fileName"]["size"]; */
 	
 	if (file_exists($target_file)) {
 		echo "Sorry, file already exists.";
@@ -49,7 +56,7 @@ if(isset($_POST["submit"])) {
 
 		$pdo = null;
 		
-	if ($uploadOk == 0) {
+	/* if ($uploadOk == 0) {
 		echo "Sorry, your file was not uploaded.";
 
 	} else {
@@ -58,7 +65,17 @@ if(isset($_POST["submit"])) {
 		} else {
 			echo "Sorry, there was an error uploading your file.";
 		}
-	}	
+	} */	
+}
+
+if ($uploadOk == 0) {
+	echo "Sorry, your file was not uploaded.";
+} else {
+	if (move_uploaded_file($_FILES["fileName"]["tmp_name"], $target_file)) {
+		echo "The file ". basename( $_FILES["fileName"]["name"]). " has been uploaded.";
+	} else {
+		echo "Sorry, there was an error uploading your file.";
+	}
 }
 
 /* $target_dir = "uploads/";
