7d470c311d6e0b3d52522e3159e50a7f35d42431
jaeneungsim <simwithyou@gmail.com>
2015-09-23 23:43:11 +1000

    ref
    ref

--- upload_doc.php
+++ upload_doc.php
@@ -3,6 +3,8 @@ $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileName"]["name"]);
 $uploadOk = 1;
 $FileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$filename = basename($_FILES["fileToUpload"]["name"]);
+$filesize = $_FILES["fileToUpload"]["size"];
 // Check if image file is a actual image or fake image
 /* if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["doc"]["tmp_name"]);
@@ -39,5 +41,58 @@ if ($uploadOk == 0) {
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
+}
+
+if(isset($_POST["submit"])) {
+    //$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+	//$imageData = file_get_contents($_FILES["fileToUpload"]["tmp_name"]);
+    //if($check !== false) {
+        //echo "File is an image - " . $check["mime"] . ".";
+        //$uploadOk = 1;
+		
+	$dbhost = "localhost";
+	$dbname	= "MEDIALYNX";
+	$dbuser	= "root";
+	$dbpass	= "root";
+	
+	session_start();
+	$userid = $_SESSION['userid'];
+	$file = "image";
+	$synopsis = "description";
+
+		try {
+			
+			
+			
+			
+					$pdo = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
+		
+			
+					
+					
+					 
+					$sql = "INSERT INTO CONTENT(USERID, CONTENTTITLE, CONTENTTYPE, SIZE, SYNOPSIS) VALUES ('$userid','$filename','$FileType','$filesize','$synopsis')";
+			
+					include 'library/closedb.php';
+					$pdo->exec($sql);
+					header("location: test2.php");
+						
+					 
+				}
+				
+			catch(PDOException $e)
+				{
+					echo $e->getMessage();
+				}
+
+				$pdo = null;
+	
+    } 
+
+
+ /* else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }	 */	
 }
 ?>
