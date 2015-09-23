988cc590a951b5a6f10f29eb372a94a2526e2da8
jaeneungsim <simwithyou@gmail.com>
2015-09-24 00:32:04 +1000

    ref
    ref

--- upload_doc.php
+++ upload_doc.php
@@ -1,6 +1,29 @@
 <?php
 
 if(isset($_POST["submit"])) {
+	
+	$target_dir = "uploads/";
+	$target_file = $target_dir . basename($_FILES["fileName"]["name"]);
+	$uploadOk = 1;
+	$FileType = pathinfo($target_file,PATHINFO_EXTENSION);
+	$filename = basename($_FILES["fileName"]["name"]);
+	$filesize = $_FILES["fileName"]["size"];
+	
+	if (file_exists($target_file)) {
+		echo "Sorry, file already exists.";
+		$uploadOk = 0;
+	}
+
+	if ($_FILES["fileName"]["size"] > 5000000) {
+		echo "Sorry, your file is too large.";
+		$uploadOk = 0;
+	}
+
+	if($FileType != "doc" && $FileType != "txt" && $FileType != "pdf") {
+		echo "Sorry, only DOC, TXT & PDF files are allowed.";
+		$uploadOk = 0;
+	}
+	
 	$dbhost = "localhost";
 	$dbname	= "MEDIALYNX";
 	$dbuser	= "root";
@@ -8,7 +31,6 @@ if(isset($_POST["submit"])) {
 	
 	session_start();
 	$userid = $_SESSION['userid'];
-	//$file = "image";
 	$synopsis = "description";
 
 		try {
@@ -26,52 +48,51 @@ if(isset($_POST["submit"])) {
 		}
 
 		$pdo = null;
-	
+		
+	if ($uploadOk == 0) {
+		echo "Sorry, your file was not uploaded.";
+
+	} else {
+		if (move_uploaded_file($_FILES["fileName"]["tmp_name"], $target_file)) {
+			echo "The file ". basename( $_FILES["fileName"]["name"]). " has been uploaded.";
+		} else {
+			echo "Sorry, there was an error uploading your file.";
+		}
+	}	
 }
 
-$target_dir = "uploads/";
+/* $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileName"]["name"]);
 $uploadOk = 1;
 $FileType = pathinfo($target_file,PATHINFO_EXTENSION);
 $filename = basename($_FILES["fileName"]["name"]);
 $filesize = $_FILES["fileName"]["size"];
-// Check if image file is a actual image or fake image
-/* if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["doc"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is a document. - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not a document.";
-        $uploadOk = 0;
-    }
-} */
-// Check if file already exists
+
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
-// Check file size
+
 if ($_FILES["fileName"]["size"] > 5000000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
-// Allow certain file formats
+
 if($FileType != "doc" && $FileType != "txt" && $FileType != "pdf") {
     echo "Sorry, only DOC, TXT & PDF files are allowed.";
     $uploadOk = 0;
 }
-// Check if $uploadOk is set to 0 by an error
+
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+
 } else {
     if (move_uploaded_file($_FILES["fileName"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileName"]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
-}
+} */
 
 /* if(isset($_POST["submit"])) {
 	$dbhost = "localhost";
