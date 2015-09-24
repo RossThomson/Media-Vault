9380b6c166de6748f90a33318babb2c511fc924d
jaeneungsim <simwithyou@gmail.com>
2015-09-24 16:57:20 +1000

    up
    up

--- download.php
+++ download.php
@@ -15,7 +15,7 @@
         exit;
     }
     
-    $query = "select CONTENTTITLE from CONTENT where num=".$_GET['num'];
+    $query = "select name, hash from ftp where num=".$_GET['num'];
     $result = $db->query($query);
     if(!$result)
     {
