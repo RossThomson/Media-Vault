e9bd9820708bb36a648794a0a12d10516815fc3a
Ross <thomson360@gmail.com>
2015-09-24 16:10:24 +1000

    Creating image gallery

--- upload_Ross_two.php
+++ upload_Ross_two.php
@@ -29,8 +29,8 @@ if ($_FILES["photo"]["size"] > 500000) {
     $uploadOk = 0;
 }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
+if($imageFileType != "jpg" && $imageFileType != "JPG" && $imageFileType != "png" && $imageFileType != "jpeg" 
+&& $imageFileType != "gif" && $imageFileType != "PNG" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
