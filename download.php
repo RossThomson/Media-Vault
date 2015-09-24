3bff5d8830933924edf994f4b0ecf6dbdeb510a4
jaeneungsim <simwithyou@gmail.com>
2015-09-24 21:42:13 +1000

    down
    down

--- download.php
+++ download.php
@@ -15,7 +15,7 @@
         exit;
     }
     
-    $query = "select CONTENTTITLE from CONTENT where num=".$_GET['num'];
+    $query = "select CONTENTID from CONTENT where num=".$_GET['num'];
     $result = $db->query($query);
     if(!$result)
     {
