e69c004e7d4fd54b159511999a13055d211c99b0
jaeneungsim <simwithyou@gmail.com>
2015-09-24 19:15:57 +1000

    down
    down

--- download.php
+++ download.php
@@ -2,11 +2,11 @@
  
     header("Content-type: text/html; charset=utf-8");
  
-    /* if(!$_GET['num'])
+    if(!$_GET['num'])
     {
         echo "<script>alert('wrong access');";
         echo "history.back();</script>";
-    } */
+    }
     
     @ $db = new mysqli('localhost', 'root', 'root', 'MEDIALYNX');
     if(mysqli_connect_errno())
@@ -15,7 +15,7 @@
         exit;
     }
     
-    $query = "select CONTENTTITLE from CONTENT where CONTENTID";
+    $query = "select name, hash from ftp where num=".$_GET['num'];
     $result = $db->query($query);
     if(!$result)
     {
@@ -24,24 +24,25 @@
     }
     $result = $result->fetch_assoc();
     
-    $dir = "uploads/";
-    $filename = $result['CONTENTTITLE'];
+    $dir = "./files/";
+    $filename = $result['name'];
+    $filehash = $result['hash'];
     
-    if(file_exists($dir.$filename))
+    if(file_exists($dir.$filehash))
     {
             header("Content-Type: Application/octet-stream");
-            header("Content-Disposition: attachment; filename="./$filename);
+            header("Content-Disposition: attachment; filename=".$filename);
             header("Content-Transfer-Encoding: binary");
-            header("Content-Length: ".filesize($dir.$filename));
+            header("Content-Length: ".filesize($dir.$filehash));
  
-            $fp = fopen($dir.$filename, "rb");
+            $fp = fopen($dir.$filehash, "rb");
             while(!feof($fp))
             {
                 echo fread($fp, 1024);
             }
             fclose($fp);
             
-            //$query = "update ftp set down=(down+1) where num=".$_GET['num'];
+            $query = "update ftp set down=(down+1) where num=".$_GET['num'];
             $result = $db->query($query);
             if(!$result)
             {    
@@ -51,7 +52,7 @@
     }
     else
     {
-            echo "<script>alert('No file);";
+            echo "<script>alert('파일이 없습니다.);";
             echo "history.back();</script>";
             exit;
     }
