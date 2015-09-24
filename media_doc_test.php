453bd49b68ddf91eab654d4fd90783fb9fd9f094
jaeneungsim <simwithyou@gmail.com>
2015-09-24 11:30:44 +1000

    test
    test

--- media_doc_test.php
+++ media_doc_test.php
@@ -69,7 +69,7 @@ header("location: Login.php");
 					{
 						$row = $result->fetch_assoc();
 						echo "<tr>";
-						echo "<td align='center'>".$row['CoNTENT']."</td>";
+						echo "<td align='center'>".$row['CONTENT']."</td>";
 						echo "<td align='left'>
 					<a href='./download.php?num=".$row['num']."'>".$row['name']."</a></td>";
 						echo "<td align='center'>".$row['SIZE']."</td>";
