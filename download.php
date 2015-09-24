abf2286b5da5942e8208feceb50fda0d59085ab8
jaeneungsim <simwithyou@gmail.com>
2015-09-24 20:10:18 +1000

    down
    down

--- download.php
+++ download.php
@@ -32,10 +32,10 @@
     {
             header("Content-Type: Application/octet-stream");
             header("Content-Disposition: attachment; filename=".$filename);
-            header("Content-Transfer-Encoding: binary");
+            //header("Content-Transfer-Encoding: binary");
             header("Content-Length: ".filesize($dir.$filename));
  
-            $fp = fopen($dir.$filename, "rb");
+            $fp = fopen($dir.$filename/*, "rb"*/);
             while(!feof($fp))
             {
                 echo fread($fp, 1024);
@@ -52,7 +52,7 @@
     }
     else
     {
-            echo "<script>alert('파일이 없습니다.);";
+            echo "<script>alert('no file);";
             echo "history.back();</script>";
             exit;
     }
