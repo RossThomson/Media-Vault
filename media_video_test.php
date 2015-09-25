eeb5a335de00bd404ba5716f55d596b58f256159
jaeneungsim <simwithyou@gmail.com>
2015-09-25 14:11:47 +1000

    test
    test

--- media_video_test.php
+++ media_video_test.php
@@ -68,6 +68,7 @@ header("location: Login.php");
 					<?php					
 					    $dir="uploads/";
 						$name=$row['CONTENTTITLE'];
+						$source = $dir.$name;
 						for($i=0; $i<$num_result; $i++)
 						{
 							$row = $result->fetch_assoc();
@@ -78,7 +79,7 @@ header("location: Login.php");
 							echo "<td align='center'>".$row['CONTENTTYPE']."</td>";
 							echo "<td align='center'>".$row['SIZE']."</td>";
 							echo "<td align='center'>
-							<video src='$dir.$name' controls autoplay></video></td>";	
+							<video src='$source' controls autoplay></video></td>";	
 							echo "<td align='center'>
 						<a href='delete_jae.php?num=".$row['CONTENTID']."'>DEL</a></td>";
 							echo "</tr>";
