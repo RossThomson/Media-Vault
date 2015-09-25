1e730dc86504e3cce5449cda4734fc54f7dc7f3e
jaeneungsim <simwithyou@gmail.com>
2015-09-25 10:15:49 +1000

    up
    up

--- upload_doc.php
+++ upload_doc.php
@@ -42,12 +42,12 @@ if(isset($_POST["submit"])) {
 	
 	session_start();
 	$userid = $_SESSION['userid'];
-	//$synopsis = "description";	
+	$type = "DOC";	
 
 		try {
 			$pdo = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
 		
-			$sql = "INSERT INTO CONTENT(USERID, CONTENTTITLE, CONTENTTYPE, SIZE, SYNOPSIS) VALUES ('$userid','$filename','$FileType','$filesize','$synopsis')";
+			$sql = "INSERT INTO CONTENT(USERID, CONTENTTITLE, CONTENTTYPE, SIZE, SYNOPSIS) VALUES ('$userid','$filename','$type','$filesize','$synopsis')";
 			
 			include 'library/closedb.php';
 			$pdo->exec($sql);
