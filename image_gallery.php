b3e6d7d38ceb66822803df4912013319509fc910
Ross <thomson360@gmail.com>
2015-09-25 12:31:27 +1000

    Making Gallery Page

--- image_gallery.php
+++ image_gallery.php
@@ -5,6 +5,8 @@
 	$dbuser	= "root";
 	$dbpass	= "root";
 	
+	$target_dir = "uploads";
+	
 	session_start();
 	$userid = $_SESSION['userid'];
 	
@@ -20,9 +22,16 @@
 	$num_result = $result->num_rows;
 	
 	if (empty($num_result))      
-    echo "nothing here"; 
+     $result_final = "t<tr><td>No Photo found</td></tr>n";     
   else {
-	  
+	  $file = row['CONTENTTITLE'];
+	  $result_final .= "<tr>nt<td align='center'>       
+      <br />       
+      <img src='$target_dir/$file' border='0'       
+        alt='$photo_caption' />       
+      <br />       
+      $photo_caption       
+      </td></tr>";     
   }      
    
 	
