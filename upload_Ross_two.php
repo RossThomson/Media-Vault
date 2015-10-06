bc9970ddcd818a9fb5029065c8b1e18a83972e0d
Ross <thomson360@gmail.com>
2015-10-07 09:49:34 +1000

    Changing upload directory

--- upload_Ross_two.php
+++ upload_Ross_two.php
@@ -3,7 +3,7 @@
 // Report all errors
 
 
-$target_dir = "uploads/";
+$target_dir = "uploads/RossThomson/";
 $target_file = $target_dir . basename($_FILES["photo"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
