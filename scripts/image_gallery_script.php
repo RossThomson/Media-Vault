80640a9f84639f3306d66fe72071c5b2d8694480
Ross <thomson360@gmail.com>
2015-10-07 12:41:26 +1000

    Changing upload directory

--- scripts/image_gallery_script.php
+++ scripts/image_gallery_script.php
@@ -46,13 +46,13 @@
 						continue;
 					}
 					
-					if ( ! file_exists('./uploads/RossTest/thumbs/'.$file) ) {
+					if ( ! file_exists('$userdirthumbs.$file') ) {
 						if ( $type == 'jpg' ) {
-							$src = imagecreatefromjpeg($userdir.$file);
+							$src = imagecreatefromjpeg('$userdir.$file');
 						} else if ( $type == 'png' ) {
-							$src = imagecreatefrompng($userdir.$file);
+							$src = imagecreatefrompng('$userdir.$file');
 						} else if ( $type == 'gif' ) {
-							$src = imagecreatefromgif($userdir.$file);
+							$src = imagecreatefromgif('$userdir.$file');
 						}
 						if ( ($oldW = imagesx($src)) < ($oldH = imagesy($src)) ) {
 							$newW = $oldW * ($max_width / $oldH);
@@ -64,11 +64,11 @@
 						$new = imagecreatetruecolor($newW, $newH);
 						imagecopyresampled($new, $src, 0, 0, 0, 0, $newW, $newH, $oldW, $oldH);
 						if ( $type == 'jpg' ) {
-							imagejpeg($new, $userdirthumbs.$file);
+							imagejpeg($new, '$userdirthumbs.$file');
 						} else if ( $type == 'png' ) {
-							imagepng($new, $userdirthumbs.$file);
+							imagepng($new, '$userdirthumbs.$file');
 						} else if ( $type == 'gif' ) {
-							imagegif($new, $userdirthumbs.$file);
+							imagegif($new, '$userdirthumbs.$file');
 						}
 						imagedestroy($new);
 						imagedestroy($src);
