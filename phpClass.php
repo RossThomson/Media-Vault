bb1689761210767cb6bb0cf5f80826d15cdfafb4
ChristophBrinkmann <christoph.brinkmann@connect.qut.edu.au>
2015-10-07 20:18:53 +1000

    phpclass and beginnings of streaming audio

---
+++ phpClass.php
@@ -0,0 +1,31 @@
+<?php
+	class ServerConn {
+	
+		function Connect() {
+			$dbhost = "localhost";
+			$dbname	= "MEDIALYNX";
+			$dbuser	= "root";
+			$dbpass	= "root";
+			try {
+				$pdo = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
+			} catch(PDOException $e){
+				echo $e->getMessage();
+			}
+			return $pdo;
+		}
+		
+		function Select($conn, $user, $table) {
+			$query = "SELECT * from '$table' where USERID = '$user'";
+			$result = $conn->exec($query);
+			return $result;
+		}
+		
+		function InsertContent($conn,$user) {
+				
+		}
+		
+		
+		
+		
+	}
+?>
---
+++ streamAudio.php
@@ -0,0 +1,28 @@
+<?php
+	PHP = new ServerConn();
+
+	if(!$_GET['file'])
+    {
+        echo "<script>alert('wrong access');";
+        echo "history.back();</script>";
+    }
+	$pod = PHP->connect();
+	
+	$dir = dirname($_SERVER['DOCUMENT_ROOT']).'';
+	$filename = $_GET['file'];
+	$file = $dir."/".$filename;
+
+	$extension = "mp3";
+	$mime_type = "audio/mpeg, audio/x-mpeg, audio/x-mpeg-3, audio/mpeg3";
+
+	if(file_exists($file)){
+		header('Content-type: {$mime_type}');
+		header('Content-length: ' . filesize($file));
+		header('Content-Disposition: filename="' . $filename.'"');
+		header('X-Pad: avoid browser bug');
+		header('Cache-Control: no-cache');
+		readfile($file);
+	}else{
+		header("HTTP/1.0 404 Not Found");
+	}
+?>
