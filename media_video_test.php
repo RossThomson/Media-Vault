de783ba4f0a41049dc0b76a8427220bceb683e0c
jaeneungsim <simwithyou@gmail.com>
2015-10-08 21:49:58 +1000

    thumb
    thumb

--- media_video_test.php
+++ media_video_test.php
@@ -80,7 +80,7 @@ header("location: Login.php");
 							echo "<td align='center'>".$row['CONTENTTYPE']."</td>";
 							echo "<td align='center'>".$row['SIZE']."</td>";
 							echo "<td align='center'>
-							<video src= 'uploads/".$row['CONTENTTITLE']."'></video></td>";	
+							<video src= 'uploads/".$row['CONTENTTITLE']."' width = '50' height = '50'></video></td>";	
 							echo "<td align='center'>
 						<a href='delete_jae.php?num=".$row['CONTENTID']."'>DEL</a></td>";
 							echo "</tr>";
