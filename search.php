753b07b9b2bc4515a29d6dd3d2ad95e283b1be2a
jaeneungsim <simwithyou@gmail.com>
2015-10-09 13:25:51 +1000

    searching test

--- search.php
+++ search.php
@@ -16,39 +16,30 @@ $query = "select * from CONTENT where CONTENTTITLE like '$key' limit 0,10";
 $result = mysql_query($query, $con);;
  
 $total = mysql_num_rows($result);
-echo("
-<table border=0 width=700>
-<tr><td align=center colspan2><h1>Search result</h1></td></tr>
-<tr><td>Key word: $key</td>
-<td align=right><a herf=searchform.php>[List]</a></td></tr>
-");
- 
-echo("
-<table border=1 width=700>
-<tr><td align=center width=50><b>File name</b><td>
-<td align=center width=50><b>Type</b><td>
-<td align=center width=50><b>Size</b><td>
-<td align=center width=50><b>Synopsis</b><td>
-</tr>
-");
-if (!$total){
-echo("<tr><td colspan=5 align=center>No search result.</td></tr>");
-} else {
-$counter=1;
-while($counter<$total):
-//echo "<td> ".$row['CONTENTTITLE']."</td>";
-/* echo $file_name=mysql_result($result,$counter,"CONTENTTITLE");
-echo $file_type=mysql_result($result,$counter,"CONTENTTYPE");
-echo $file_size=mysql_result($result,$counter,"SIZE");
-echo $synopsis=mysql_result($result,$counter,"SYNOPSIS");
-$counter = $counter + 1; */
-echo mysql_result($result,$counter,"CONTENTTITLE");
-echo mysql_result($result,$counter,"CONTENTTYPE");
-echo mysql_result($result,$counter,"SIZE");
-echo mysql_result($result,$counter,"SYNOPSIS");
-$counter = $counter + 1;
-endwhile;
-echo("</table>");
-}
+?>
+<table border='1' align="center">
+	<thead>
+		<tr>
+			<th width="250">FILE</th>
+			<th width="150">TYPE</th>
+			<th width="200">SIZE</th>
+			<th width="50">SYNOPSIS</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php					
+	for($i=0; $i<$total; $i++) {
+		$row = $result->fetch_assoc();
+		echo "<tr>";
+		echo "<td align='left'>".$row['CONTENTTITLE']."</td>";
+		echo "<td align='center'>".$row['CONTENTTYPE']."</td>";
+		echo "<td align='center'>".$row['SIZE']."</td>";
+		echo "<td align='center'>".$row['SYNOPSIS']."</td>";
+		echo "</tr>";
+	}
+	?>
+	</tbody>
+</table>
+<?php
 mysql_close($con);
 ?>
