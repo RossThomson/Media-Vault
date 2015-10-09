32a384ea8ad0c2ec7581e8e87cc113fbe043bd47
jaeneungsim <simwithyou@gmail.com>
2015-10-09 10:56:20 +1000

    test

--- search.php
+++ search.php
@@ -1,31 +1,32 @@
 <?
-$field=$_POST['CONTENTTITLE'];
+$field = $_POST['field']; 
 $key = $_POST['key'];
- 
+
 if(!$key){
  echo("<script>
- window.alert('Enter the key words');
+ window.alert('Enter words');
  history.go(-1);
  </script>");
  exit;
 }
-
 $con = mysql_connect("localhost", "root", "root");
 mysql_select_db("MEDIALYNX",$con);
 mysql_query("set names 'euckr'");
 $result=mysql_query("select * from CONTENT where $field like '%$key%'", $con);
 $total = mysql_num_rows($result);
+
 echo("
 <table border=0 width=700>
 <tr><td align=center colspan2><h1>Media files</h1></td></tr>
-<tr><td>검색어: $key</td>
-<td align=right><a herf=searchform.php>[전체목록]</a></td></tr>");
+<tr><td>Key word:$key</td>
+<td align=right><a herf=searchform.php>[List]</a></td></tr>
+");
 echo("
    <table border=1 width=700>
-   <tr><td align=center  width=50><b>File name</b><td>
-   <td align=center  width=50><b>Type</b><td>
-   <td align=center  width=50><b>Size</b><td>
-   <td align=center  width=50><b>Synopsis</b><td>
+   <tr><td align=center  width=50><b>File name</b><?td>
+   <td align=center  width=100><b>Type</b><td>
+   <td align=center  width=20><b>Size</b><td>
+   <td align=center  width=500><b>Synopsis</b><td>
    </tr>
   ");
   if (!$total){
@@ -37,7 +38,7 @@ echo("
     $ProductName=mysql_result($result,$counter,"CONTENTTYPE");
     $Sex=mysql_result($result,$counter,"SIZE");
     $Type=mysql_result($result,$counter,"SYNOPSIS");
-  
+   $t="";
    
     $counter = $counter + 1;
     endwhile;
