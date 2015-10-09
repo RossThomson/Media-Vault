93ab0569c9c5aeae746b63a597d220ac78f81599
Ross <thomson360@gmail.com>
2015-10-09 12:25:53 +1000

    Changed directory of upload pages

--- media_doc.php
+++ media_doc.php
@@ -110,8 +110,7 @@ header("location: Login.php");
 							echo "<td align='center'>".$row['SYNOPSIS']."</td>";
 							echo "<td align='center'>
 						<a href='delete_jae.php?num=".$row['CONTENTID']."'>DEL</a></td>";
-							echo '<td align='center'>
-						<a href= "' . $userdir.$row['CONTENTTITLE']. '">View</a></td>';
+							echo '<td align='center'><a href= "' . $source. '">View</a></td>';
 							echo "</tr>";
 						}
 						$db->close();
