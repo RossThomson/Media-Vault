2b21e56238e50be0298acdf12a34b4a4e884c17e
jaeneungsim <simwithyou@gmail.com>
2015-10-09 13:28:10 +1000

    searching test

--- search.php
+++ search.php
@@ -16,30 +16,34 @@ $query = "select * from CONTENT where CONTENTTITLE like '$key' limit 0,10";
 $result = mysql_query($query, $con);;
  
 $total = mysql_num_rows($result);
-?>
-<table border='1' align="center">
-	<thead>
-		<tr>
-			<th width="250">FILE</th>
-			<th width="150">TYPE</th>
-			<th width="200">SIZE</th>
-			<th width="50">SYNOPSIS</th>
-		</tr>
-	</thead>
-	<tbody>
-	<?php					
-	for($i=0; $i<$total; $i++) {
-		$row = $result->fetch_assoc();
-		echo "<tr>";
-		echo "<td align='left'>".$row['CONTENTTITLE']."</td>";
-		echo "<td align='center'>".$row['CONTENTTYPE']."</td>";
-		echo "<td align='center'>".$row['SIZE']."</td>";
-		echo "<td align='center'>".$row['SYNOPSIS']."</td>";
-		echo "</tr>";
-	}
-	?>
-	</tbody>
-</table>
-<?php
+echo("
+<table border=0 width=700>
+<tr><td align=center colspan2><h1>Search result</h1></td></tr>
+<tr><td>Key word: $key</td>
+<td align=right><a herf=searchform.php>[List]</a></td></tr>
+");
+ 
+echo("
+<table border=1 width=700>
+<tr><td align=center width=50><b>File name</b><td>
+<td align=center width=50><b>Type</b><td>
+<td align=center width=50><b>Size</b><td>
+<td align=center width=50><b>Synopsis</b><td>
+</tr>
+");
+if (!$total){
+echo("<tr><td colspan=5 align=center>No search result.</td></tr>");
+} else {
+$counter = 0;
+while($counter<$total):
+//echo "<td> ".$row['CONTENTTITLE']."</td>";
+$file_name=mysql_result($result,$counter,"CONTENTTITLE");
+$file_type=mysql_result($result,$counter,"CONTENTTYPE");
+$file_size=mysql_result($result,$counter,"SIZE");
+$synopsis=mysql_result($result,$counter,"SYNOPSIS");
+$counter = $counter + 1;
+endwhile;
+echo("</table>");
+}
 mysql_close($con);
 ?>
