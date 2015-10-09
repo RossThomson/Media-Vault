566930dba8899e040c432513d0d83efa2f515773
jaeneungsim <simwithyou@gmail.com>
2015-10-09 10:31:10 +1000

    searching test

---
+++ searchform.php
@@ -0,0 +1,14 @@
+<html>
+<table>
+<td><td align=center>
+<form method=post action=search.php>
+<select name=field>
+<option value=Code>File name</option>
+<option value=ProductName>Type</option>
+<option value=Sex>Size</option>
+<option value=Type>Synopsis</option>
+</select>
+<input type=text name=key size=20>
+<input type=submit value=검색>
+</td>
+</html>
