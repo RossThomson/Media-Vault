3779e578245e7422fc9a5b789d0f1fa7fefd3938
Ross <thomson360@gmail.com>
2015-10-13 11:58:44 +1000

    Ceating toggle view for image gallery page

--- image_gallery_test.php
+++ image_gallery_test.php
@@ -66,6 +66,7 @@ img {
 <?php getPictures(); ?>
 
 
+
 <script type="text/javascript" src="js/prototype.js"></script>
 <script type="text/javascript" src="js/scriptaculous.js?load=effects,builder"></script>
 <script type="text/javascript" src="js/lightbox.js"></script>
