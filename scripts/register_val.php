063cea208ab51b3af92c1f419f4fbdc1bd7725a7
priyana <ppriyanka211@yahoo.com>
2015-10-16 15:40:15 +1000

    Update register_val.php

--- scripts/register_val.php
+++ scripts/register_val.php
@@ -68,17 +68,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	
 }
 
-<?php
-if(isset($_Request['email']))
-{
-		$admin_email="ndhimar@yahoo.co.in";
-		$email=$_Request['email'];
-		mail($admin_email,"Test",Hello,"From:".$email);
-		echo("Done")
-}
-}
-
-?>	
+	
 
 
 
