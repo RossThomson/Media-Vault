19a9f1ae76b5607dc9fdb8580afcb18544a5326f
priyana <ppriyanka211@yahoo.com>
2015-10-16 11:22:42 +1000

    Update index.php

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ header("location: media.php");
 			<div id = "register">
 			<h2> "Your own online Media Vault"</h2>
 			<a class="btn btn-alt" href="register.php">Register Now</a>
+			<a  href="newtest.php">Register Now</a>
 			</div>
 					<footer class="footer_absolute">
 					<span id="jae_design-by">Design by Media lynx</span> 
