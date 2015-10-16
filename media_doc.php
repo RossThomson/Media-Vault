6c95cdf3baa74e38b00ca0d2d472f017a0104cae
Ross <thomson360@gmail.com>
2015-10-16 14:20:30 +1000

    Adding javascript popups for delete button

--- media_doc.php
+++ media_doc.php
@@ -23,6 +23,7 @@ header("location: Login.php");
 	<meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
     <link rel="stylesheet" href="styles/styles.css">
 	<script type="text/javascript" src="scripts/upload_val.js"></script>
+	<script type="text/javascript" src="scripts/confirm.js"></script>
 </head>
 
 <body>
@@ -92,8 +93,7 @@ header("location: Login.php");
                 <a href='download.php?num=".$row['CONTENTID']."'>".$row['CONTENTTITLE']."</a></td>";
                     echo "<td align='center'>".$row['SIZE']."</td>";
 					echo "<td align='center'>".$row['SYNOPSIS']."</td>";
-                    echo "<td align='center'>
-                <a href='delete_jae.php?num=".$row['CONTENTID']."' onclick="return confirm("Are you sure?")">DEL</a></td>";
+                    echo "<td align='center'><a href='delete_jae.php?num=".$row['CONTENTID']."' class = 'confirmation'>DEL</a></td>";
 					echo "<td align='center'>
 					<a href='". $target_dir.$row['CONTENTTITLE']."'>View</a></td>";
 
