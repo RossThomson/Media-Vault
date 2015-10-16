2183f2d302b1afcc1f566ff632e8ce2a79d4f909
ChristophBrinkmann <christoph.brinkmann@connect.qut.edu.au>
2015-10-16 21:05:30 +1000

    changing header bootstrap

--- header_bootstrap.php
+++ header_bootstrap.php
@@ -28,10 +28,10 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
+		  <a href="index_bootstrap.php" class="navbar-brand"><img alt="brand" src="graphics/logo.jpg" class="img-responsive"></a>
 		</div>
         <div class="navbar-collapse collapse" id="navbar">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="index_bootstrap.php"><img src="graphics/logo.jpg" height="50" width="50"></a></li>
             <li><a href="about.php"><img src="graphics/about.png"></a><a href="about.php">About</a></li>
             <li><a href="help.php"><img src="graphics/help.png"></a><a href="help.php">Help</a></li>
           </ul>
