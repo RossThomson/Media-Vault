e0e9632731b24f27b5b928680cd352cc992e849c
priyana <ppriyanka211@yahoo.com>
2015-10-16 15:52:53 +1000

    Update register.php

--- register.php
+++ register.php
@@ -1,3 +1,22 @@
+	<?php 
+if(isset($_POST['submit'])){
+    $to = "ppriyanka211@yahoo.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['firstname'];
+    $last_name = $_POST['lastname'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" ;
+    $message2 = "Here is a copy of your message " . $first_name . ";
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -38,25 +57,7 @@
 	</form>
 	
 	</div>
-	<?php 
-if(isset($_POST['submit'])){
-    $to = "ppriyanka211@yahoo.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['firstname'];
-    $last_name = $_POST['lastname'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" ;
-    $message2 = "Here is a copy of your message " . $first_name . ";
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
-?>
 	<footer class="footer_relative">
 		<span id="jae_design-by">Design by Media lynx</span> 
 		Copyright &copy; Media Lynx 2015.
