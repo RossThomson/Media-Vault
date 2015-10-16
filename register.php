ca56cbf6b6ebc4b58b9eff708101f1941b51e873
priyana <ppriyanka211@yahoo.com>
2015-10-16 13:46:50 +1000

    Update register.php

--- register.php
+++ register.php
@@ -5,33 +5,25 @@
 	<meta charset = "utf-8">
 	<link rel="stylesheet" href="styles/styles.css">
 	<script type="text/javascript" src="scripts/register_val.js"></script>
-	<?php include 'scripts/register_val.php';?>
+	<?php include 'scripts/register_val.php';?> 
 	</head>
 	
 	<body>
-	
+		
+		
 	<div class="wrapper">
 	
 			<header>
-	
-				<nav>
-					
-					<a href="index.php"><img src="graphics/logo.jpg"></a>
-			
-					<ul>
-						
-						<li><a href="Login.php">Login</a></li>
-						<li><a href="about.php">About</a></li>
-						<li><a href="help.php">Help</a></li>
-					</ul>			
-				</nav>
+				<?php include 'header.php'; ?>
 			</header>
+			
+			
 	
 	<!--FORM FOR THE USER TO FILL OUT-->
 	<div id="register_form">
 	
 	<h1>Register For An Account</h1>
-	<form id="regform" name="Register_form" onsubmit="scripts/register_val.js" action="scripts/register_val.php"  method="post">
+	<form id="regform" name="Register_form" onsubmit="scripts/register_val.js" action="scripts/register_val.php"  method="Post">
 		<fieldset>
 		<p><span id="error_message"><?php session_start(); echo $_SESSION['error'];?></span></p>
 		<p><label>First Name: </label><input type="text" name="firstname" id="name" required onchange="firstNameVal();">*<span id ="namemessage"></span></p>
@@ -41,15 +33,16 @@
 		<p><label>Secret Answer: </label><input type="text" name="secretanswer" required>*</p>
 		<p><label for="password">Password: </label><input type="password" min = "6" max = "15" id="password" name="password" required>*</p>
 		<p><label for="cPassword">Confirm Password: </label><input type="password" id="cPassword" name="cPassword" required onkeyup ="checkPass(); return false;">*<span id="confirmMessage" class="confirmMessage"></span></p>
-		<p><input class="btn btn-alt" type = "submit" name = "submit" id = "submit" value = "Submit">
+		<p><input class="btn btn-alt" type = "submit" name = "Submit" id = "Submit" value = "Submit">
 		</fieldset>
-		</form>
-<?php
+	</form>
+	<?php
 if (isset($_POST['submit']))
 {
+echo "test";
    session_start();
    mysql_connect('localhost','root','root');
-   mysql_select_db("MEDIALYNX");;
+   mysql_select_db("Media-Vault");;
 	
 	$FirstName = mysql_real_escape_string($_POST['FirstName']);
 	$LastName = mysql_real_escape_string($_POST['LastName']);
@@ -76,7 +69,6 @@ if (isset($_POST['submit']))
 		echo "Registration complete! Please confirm your mail ";
 	}
 ?>	
-	
 	</div>
 	
 	<footer class="footer_relative">
