37b238ef59e31b2f2dcb83d89068d5514a0ffea1
ChristophBrinkmann <christoph.brinkmann@connect.qut.edu.au>
2015-10-18 07:45:59 +1000

    bootstrap straping

--- header_bootstrap.php
+++ header_bootstrap.php
@@ -36,7 +36,7 @@
             <li><a href="help.php"><span class="glyphicon glyphicon-book"></span>Help</a></li>
           </ul>
 		  <ul class="nav navbar-nav navbar-right">
-			<li><a href="<?php echo $login;?>_bootstrap.php"><label class="label-inverse"><?php echo $login;?></label></a></li>
+			<li><a href="<?php echo $login;?>_bootstrap.php"><label class="label"><?php echo $login;?></label></a></li>
   			<li><span id = "sign_in_info"><?php echo $welcome;?> <?php echo $_SESSION['first_name'];?></span></li>
 		  </ul>
         </div><!--/.nav-collapse -->
