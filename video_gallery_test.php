7cdaa71d14a9505e2ef1cb49a810b8f067c1a3ef
Ross <thomson360@gmail.com>
2015-10-20 16:57:02 +1000

    Building video gallery

--- video_gallery_test.php
+++ video_gallery_test.php
@@ -81,7 +81,7 @@ header("location: Login.php");
 						{
 							$row = $result->fetch_assoc();
 						
-							echo '<a href="'. $target_dir .'/ '.$row['CONTENTTITLE']. ' "><img src="uploads/RossTestTwo/fish.jpg" alt="fish"></a>'
+							echo '<a href="'. $target_dir.$row['CONTENTTITLE']. ' "><img src="uploads/RossTestTwo/fish.jpg" alt="fish"></a>'
 					
 						
 						}
