91ce3d639e6ebd9ceb1c075cd37b90e343354f00
ChristophBrinkmann <christoph.brinkmann@connect.qut.edu.au>
2015-10-21 14:22:29 +1000

    should finish index page

--- index_bootstrap.php
+++ index_bootstrap.php
@@ -37,9 +37,77 @@ header("location: media.php");
 			<h1>Welcome to Media Lynx!</h1>
 			<p>It's like Netflix, but for your own content!</p>
 			<p>
-			  <a class="btn btn-lg btn-primary" href="register_bootstrap.php" role="button">Register now »</a>
+			  <a href="#" data-toggle = "modal" data-target="#register" role="button">Register now</a>
 			</p>
 		 </div>
+		<div id="register" class="modal" roll="dialog" tabindex="-1">
+			<div class="modal-dialog modal-lg">
+				<div class="modal-content">
+					<div class=""modal-header">
+						<button type="button" class="close" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></button>
+						<h1>Register</h1>
+					</div>
+					<div class="modal-body">
+						<form class="form form-horizontal" role="form" name="Register_form" onsubmit="scripts/register_val.js" action="scripts/register_val.php"  method="post">
+							<p><span id="error_message"><?php session_start(); echo $_SESSION['error'];?></span></p>
+							<div class="form-group">
+								<label for="firstname" class="col-sm-2 control-label">First Name*: </label>
+								<div class="col-sm-9">
+									<input type="text" class="form-control" name="firstname" id="name" required onchange="firstNameVal();"><span id ="namemessage"></span>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="lastname" class="col-sm-2 control-label">Last name*: </label>
+								<div class="col-sm-9">
+									<input type="text" class="form-control" name="lastname" id="surname" required onchange="surnameVal();"><span id ="surnamemessage"></span>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="email" class="col-sm-2 control-label">Email Address*: </label>
+								<div class="col-sm-9">
+									<input type="text"class="form-control" name="email" id="email" required onchange ="checkEmail();"><span id ="emailmessage"></span>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="secretquestion" class="col-sm-2 control-label">Secret Question*: </label>
+								<div class="col-sm-9">
+									<input type="text" class = "form-control" name="secretquestion" required>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="secretanswer" class="col-sm-2 control-label">Secret Answer*: </label>
+								<div class="col-sm-9">
+									<input type="text" class="form-control" name="secretanswer" required>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="password" class="col-sm-2 control-label">Password*: </label>
+								<div class="col-sm-9">
+									<input type="password" class="form-control" min = "6" max = "15" id="password" name="password" required>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="cPassword" class="col-sm-2 control-label">Confirm Password*: </label>
+								<div class="col-sm-9">
+									<input type="password" class="form-control" id="cPassword" name="cPassword" required onkeyup ="checkPass(); return false;"><span id="confirmMessage" class="confirmMessage"></span>
+								</div>
+							</div>
+							<div>
+								<input class="btn btn-lg btn-primary" type = "submit" name = "Submit" id = "Submit" value = "Submit">
+							</div>
+							</fieldset>
+						</form>
+					</div>
+					<div class="modal-footer">
+						<div class="col-sm-12">
+							<button class="btn" data-dismiss="modal">Close</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		
 		
 		<footer class="footer_absolute">
 		<span id="jae_design-by">Copyright &copy; Media Lynx 2015</span> 
