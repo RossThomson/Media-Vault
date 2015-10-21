5631cb215ec93b3449c3a9aeda5192240c5a9f79
jaeneungsim <simwithyou@gmail.com>
2015-10-21 17:29:20 +1000

    searching

--- searchform_test.php
+++ searchform_test.php
@@ -1,12 +1,6 @@
 <html>
-<form class="navbar-form navbar-left input-s-lg m-t m-l-n-xs hidden-xs" role="search" method="post" action="search.php">
-        <div class="form-group">
-          <div class="input-group">
-            <span class="input-group-btn">
-              <button type="submit" class="btn btn-sm bg-white btn-icon rounded"><i class="fa fa-search"></i></button>
-            </span>
-            <input type="text" name="search" class="form-control input-sm no-border rounded" placeholder="Search songs, video...">
-          </div>
-        </div>
-      </form>
+	<form role="search" method="post" action="search.php">
+		<button type="submit"></button>
+		<input type="text" name="search" class="form-control input-sm no-border rounded" placeholder="Search songs, video...">
+	</form>
 </html>
