f72f65af8f9ea88ea41ed592a8064fa1c3b4efc4
priyana <ppriyanka1708@gamil.com>
2015-10-22 13:41:31 +1000

    checkbox

--- php_checkbox.php
+++ php_checkbox.php
@@ -1,74 +1,29 @@
-<?php
+<?php  
+define("MYSQLUSER","root");
+define("MYSQLPASS","root");
+define("HOSTNAME","localhost");
+define("MYSQLDB","MEDIALYNX");
+if(isset($_POST['sub']))  
+{  
+	$conn = new mysqli("localhost", "root", "root", "MEDIALYNX");
+		if($conn -> connect_error) {
+			die('Connect Error: ' . $conn -> connect_error);
+		} 	
 
-
-
-session_start();
-	
-$firstname  = $_SESSION['first_name'];
-$lastname = $_SESSION['last_name'];
-$userid = $_SESSION['userid'];
-$dir = "uploads/";
-$target_dir = $dir.$firstname.$lastname."/";
-
-$target_file = $target_dir . basename($_FILES["checkbox"]["name"]);
-$uploadOk = 1;
-$FileType = pathinfo($target_file,PATHINFO_EXTENSION);
-$filename = basename($_FILES["checkbox"]["name"]);
-$filesize = $_FILES["checkbox"]["size"];
-$synopsis = $_POST['ref'];
-
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-
-if ($_FILES["checkbox"]["size"] > 5000000000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-
-if($FileType != "mp3" && $FileType != "mp2") {
-    echo "Sorry, only MP3 files are allowed.";
-    $uploadOk = 0;
-}
-
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-
-} else {
-    if (move_uploaded_file($_FILES["checkbox"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["checkbox"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
-
-if(isset($_POST["submit"])) {
-	$dbhost = "localhost";
-	$dbname	= "MEDIALYNX";
-	$dbuser	= "root";
-	$dbpass	= "root";
-	
-	
-	$type = "MUSIC";
-
-		try {
-			$pdo = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
-		
-			$sql = "INSERT INTO PLAYLIST(USERID, CONTENTTITLE, CONTENTTYPE) VALUES ('$userid','$filename','$type')";
-			
-			//include 'library/closedb.php';
-			$pdo->exec($sql);
-			//header("location: upload_doc.php");
-		}
-				
-		catch(PDOException $e){
-			echo $e->getMessage();
-		}
-
-		$pdo = null;
-			header("location: media_music.php");
-			echo "<script>alert('uploaded');";
-		
-}
-?>
+$checkbox1=$_POST['techno'];  
+$chk="";  
+foreach($checkbox1 as $chk1)  
+   {  
+      $chk .= $chk1.",";  
+   }  
+$in_ch=mysqli_query($con,"insert into PLAYLIST(USERID, CONTENTTITLE, CONTENTTYPE) values ('$chk')");  
+if($in_ch==1)  
+   {  
+      echo'<script>alert("Inserted Successfully")</script>';  
+   }  
+else  
+   {  
+      echo'<script>alert("Failed To Insert")</script>';  
+   }  
+}  
+?>  
