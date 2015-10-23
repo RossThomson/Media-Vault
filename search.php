d70d50773fc37db782fb0e39657fe0fc22d08729
jaeneungsim <simwithyou@gmail.com>
2015-10-23 13:22:17 +1000

    search

--- search.php
+++ search.php
@@ -17,22 +17,8 @@ $query = "select * from CONTENT where CONTENTTITLE like '$key' limit 0,10";
 $result = mysql_query($query, $con); 
 $total = mysql_num_rows($result);
 
-echo("
-<table border=0 width=700>
-<tr><td align=center colspan2><h1>Result</h1></td></tr>
-<tr><td>Keyword: $key</td>
-<td align=right><a herf=searchform.php>[List]</a></td></tr>
-");
-echo("
-<table border=1 width=700>
-<tr><td align=center width=50><b>Name</b><td>
-<td align=center width=50><b>Type</b><td>
-<td align=center width=50><b>Size</b><td>
-<td align=center width=50><b>Synopsis</b><td>
-</tr>
-");
 if (!$total){
-echo("<tr><td colspan=5 align=center>There is no search result</td></tr>");
+echo("There is no search result");
 } else {
 $counter=0;
 while($counter<$total):
@@ -40,7 +26,7 @@ while($counter<$total):
 //$student_grade=mysql_result($result,$counter,"CONTENTTYPE");
 //$student_div=mysql_result($result,$counter,"SIZE");
 //$student_no=mysql_result($result,$counter,"SYNOPSIS");
-echo ("<tr><td colspan=5 align=center>".$row['CONTENTTITLE']."</td></tr>");
+echo (($result,$counter,"CONTENTTITLE"));
 
 $counter = $counter + 1;
 endwhile;
