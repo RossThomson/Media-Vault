4eb3c502c6f683990fc9a82b1b8e67a2a785c9cc
Ross <thomson360@gmail.com>
2015-10-23 14:19:51 +1000

    Fixing javascript upload validation

--- media_music.php
+++ media_music.php
@@ -124,7 +124,6 @@ header("location: Login.php");
 		
 	<br><br><br>	
 	<footer class="footer_relative">
-	<span id="jae_design-by">Design by Media lynx</span> 
 		Copyright &copy; Media Lynx 2015.
 	</footer>
 
