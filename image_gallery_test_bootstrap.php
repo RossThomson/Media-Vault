ec6333edbd4f1f135630619f687366eafe4dd771
ChristophBrinkmann <christoph.brinkmann@connect.qut.edu.au>
2015-10-23 14:47:06 +1000

    just testing gallery with bootstrap

--- image_gallery_test_bootstrap.php
+++ image_gallery_test_bootstrap.php
@@ -13,9 +13,21 @@ header("location: Login.php");
 <meta http-equiv="Content-Type" content="text/html; charset=UFT-8" />
 <title>Pictures</title>
 <link rel="stylesheet" href="styles/lightbox.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="styles/styles.css">
-<script src="js/jquery-1.11.3.min.js"></script>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<meta name="description" content="">
+<meta name="author" content="">
+<!--<link rel="icon" href="http://getbootstrap.com/favicon.ico">-->
+<!--<title>Media Lynx</title>-->
+<!-- Bootstrap core CSS -->
+<link href="bootstrap-3.3.5-dist/css/bootstrap.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="./bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+<!-- Custom styles for this template -->
+<link href="http://getbootstrap.com/examples/navbar-fixed-top/navbar-fixed-top.css" rel="stylesheet">
 <script type="text/javascript" src="scripts/upload_val.js"></script>
+<script src="js/jquery-1.11.3.min.js"></script>
 <?php include 'scripts/image_gallery_script.php'; ?>
 
 <style type="text/css">
@@ -33,26 +45,23 @@ header("location: Login.php");
 </head>
 <body class="bodyMedia">
 
-<div class="wrapper">
-	<header>
-		<?php include 'header_bootstrap.php'; ?>
-		<span id="sign_in_info"></span>
-		<div id="media2_header_inside">
-			<!-- <a href="index.php">
-				<img src="graphics/logo.jpg">
-			</a> -->
-			<ul>
-				<li><a href="media_playlist.php">Playlists</a></li>
-				<li><a href="media_doc.php">Docs</a></li>
-				<li><a class="active" href="image_gallery_test.php">Photos</a></li>
-				<li><a href="media_music.php">Music</a></li>
-				<li><a href="media_video.php">Videos</a></li>
-				<li><a href="media_all.php">All files</a></li>
-			</ul>		
-		</div>
-	</header>
+<header>
+	<?php include 'header_bootstrap.php'; ?>
+<div class="container-fluid">
+	<div class = "container">
+		<ul class="nav nav-tabs nav-justified">
+			<li><a href="media_all_bootstrap.php">All files</a></li>
+			<li><a href="media_playlist.php">Playlists</a></li>
+			<li><a href="media_doc_bootstrap.php">Docs</a></li>
+			<li class="active disabled"><a class="active" href="media_photo_bootstrap.php">Photos</a></li>
+			<li><a href="media_music.php">Music</a></li>
+			<li><a href="media_video.php">Videos</a></li>
+		</ul>		
+	</div>
 </div>
-	<script type="text/javascript" src="scripts/check_for_empty_field.js"></script>
+</header>
+
+<script type="text/javascript" src="scripts/check_for_empty_field.js"></script>
 
 <form class="upload_form" action="upload_Ross_two.php" method="post" enctype="multipart/form-data" onsubmit="return checkImageFile(this);">
     Select an image to upload:
