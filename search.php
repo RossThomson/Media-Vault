4fd5cc8634c972df81dc5a020ba8e0db40a3264b
jaeneungsim <simwithyou@gmail.com>
2015-10-23 11:43:36 +1000

    searching

--- search.php
+++ search.php
@@ -1,44 +1,46 @@
-<?php
-include "conn.php";
-
-$output = '';
-if(isset($_POST['search'])){
-	if($_POST['search']==" "){	
-	$output ="<script>window.alert('Please enter keywords')</script>";
-	} else{
-	
-    $searchq = $_POST['search'];
-	$searchq = preg_replace("#[^0-9a-z]#i","",$searchq);
-	
-	$query = mysql_query("SELECT * FROM CONTENT WHERE CONTENTTITLE LIKE '%$searchq%'") or die("could not search");
-	$count = mysql_num_rows($query);
-		if($count == 0){
-			$output = 'There was no search result';
-		} else {
-			while($row = mysql_fetch_array($query)){
-				$name = $row['CONTENTTITLE'];
-				//$fileaddress=$row['address'];
-				//$tmp=$row['address'];
-				$fileid=$row['CONTENTID'];
-				//$file=pathinfo($fileaddress);
-				$filetype=$file['CONTENTTYPE'];
-
-				if( $filetype=='pdf'){
-					$output .= "<div><a href='$tmp' target='blank'><img src='/img/pdf.png'><br>".$name."</a></div><br/><br/>";
-				}
-				if($filetype=='VIDEO'){
-					$output.="<video width='880' height='480' controls>
-					<source src='uploads/JaeSim/ga.mp4' type='video/mp4'></video><br/>".$row['CONTENTTITLE']."<br/><br/>";
-			    }
-				if($filetype=='mp3'){
-					$output.="<audio width='300' height='220' controls>
-					<source src='".$row['address']."' type='video/mp4'></audio><br/>".$row['name']."<br/><br/>";
-				}
-				if($filetype=='jpg'){
-					$output .= "<div><a href='$tmp' target='blank'><img src='$tmp'><br>".$name."</a></div><br/><br/>";
-				}		
-			}
-		}
-	}
+<?
+$field=$_POST['field'];
+$key = $_POST['key'];
+if(!$key){
+echo("<script>
+window.alert('Input keywords');
+history.go(-1);
+</script>");
+exit;
 }
+ 
+$con = mysql_connect("localhost", "root", "root");
+mysql_select_db("MEDIALYNX",$con);
+mysql_query("'$student_name' 'euckr'");
+$result = mysql_query("select * from CONTENT where CONTENTTITLE like '%$key%' limit 0,10", $con);
+ 
+$total = mysql_num_rows($result);
+echo("
+<table border=0 width=700>
+<tr><td align=center colspan2><h1>Result</h1></td></tr>
+<tr><td>Keyword: $key</td>
+<td align=right><a herf=searchform.php>[List]</a></td></tr>
+");
+echo("
+<table border=1 width=700>
+<tr><td align=center width=50><b>Name</b><td>
+<td align=center width=50><b>Type</b><td>
+<td align=center width=50><b>Size</b><td>
+<td align=center width=50><b>Synopsis</b><td>
+</tr>
+");
+if (!$total){
+echo("<tr><td colspan=5 align=center>검색된 내용이 없습니다.</td></tr>");
+} else {
+$counter=0;
+while($counter<$total):
+$case_alias_year=mysql_result($result,$counter,"CONTENTTITLE");
+$student_grade=mysql_result($result,$counter,"CONTENTTYPE");
+$student_div=mysql_result($result,$counter,"SIZE");
+$student_no=mysql_result($result,$counter,"SYNOPSIS");
+$counter = $counter + 1;
+endwhile;
+echo("</table>");
+}
+mysql_close($con);
 ?>
