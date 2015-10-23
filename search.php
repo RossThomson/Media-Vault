f88f4532486fb5ffd39df4b30dbe3f63379fcacb
jaeneungsim <simwithyou@gmail.com>
2015-10-23 12:46:07 +1000

    search

--- search.php
+++ search.php
@@ -40,7 +40,7 @@ while($counter<$total):
 //$student_grade=mysql_result($result,$counter,"CONTENTTYPE");
 //$student_div=mysql_result($result,$counter,"SIZE");
 //$student_no=mysql_result($result,$counter,"SYNOPSIS");
-echo "<tr><td> ".$row['CONTENTTITLE']." </td></tr>";
+echo .$row['CONTENTTITLE'].;
 
 $counter = $counter + 1;
 endwhile;
