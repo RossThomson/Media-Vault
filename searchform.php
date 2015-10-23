a6dc0ca81062add62a7bb21e3a11315c4cb27ada
jaeneungsim <simwithyou@gmail.com>
2015-10-23 11:05:42 +1000

    searching

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="post" action="search.php">
-    <button type="submit"></button>
-    <input type="text" name="search">
+<form method="post" action="search.php">
+    <input name="search" type="text" />
+    <input type="submit" value="Submit" name="submit"/>
 </form>
