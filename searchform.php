91a139803cd40beb421866f9d2e09feb8c2dbe1c
jaeneungsim <simwithyou@gmail.com>
2015-10-23 11:31:30 +1000

    searching

--- searchform.php
+++ searchform.php
@@ -3,7 +3,7 @@
 <td><td align=center>
 <form method=post action=search.php>
 <select name=field>
-<option value=student_name>성명</option>
+<option value=student_name>File name</option>
 </select>
 <input type=text name=key size=20>
 <input type=submit value=search>
